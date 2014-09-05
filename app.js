2cc38920fc53eedabd35866b134226867b8f197f
Anders Jansson <nowevenbetter@gmail.com>
2014-09-05 13:31:34 +0200

    aw yiss

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.htm');
 });
 
+painting = new Painting();
+
 io.on('connection', function(socket){
   console.log('client connected');
-
+  console.log(painting.getFullPainting());
   socket.on('chatMessage', function(msg){
     var input = JSON.parse(msg);
     var chatString = input.nick + ": " + input.msg;
@@ -19,12 +21,34 @@ io.on('connection', function(socket){
 
   socket.on("draw", function(msg){
     socket.broadcast.emit("otherUserDrawing", msg);
-    console.log(JSON.parse(msg));
+    painting.saveBrushStroke(JSON.parse(msg));
+  });
+
+  socket.on("destroy", function(){
+    painting.removeAll();
   });
 
 });
 
+function Painting()
+{
+  this.paintArray = new Array();
+}
 
+  Painting.prototype.saveBrushStroke = function(data)
+  {
+    this.paintArray.push(data);
+  }
+
+  Painting.prototype.getFullPainting = function()
+  {
+    return JSON.stringify(this.paintArray);
+  }
+
+  Painting.prototype.removeAll = function()
+  {
+    this.paintArray = [];
+  }
 
 http.listen(8080, function(){
   console.log('listening on port 8080');
