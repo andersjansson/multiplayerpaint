af8b186bd775582c7e8e2efe00320dee526aa700
Alex Mkay <Alex@Alleh.io>
2014-11-07 11:34:09 +0100

    remove some stuff api routes

--- routes/routes.js
+++ routes/routes.js
@@ -34,7 +34,7 @@ module.exports = function(app, passport) {
 
 		
 		app.post('/signup', passport.authenticate('local-signup', {
-			successRedirect : '/profile', 
+			successRedirect : '/', 
 			failureRedirect : '/signup', 
 			failureFlash : true 
 		}));
