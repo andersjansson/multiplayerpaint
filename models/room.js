661775dbfbed3629e5cafd343afea625c5044170
Alex Mkay <Alex@Alleh.io>
2014-11-10 14:55:47 +0100

    rooooouuuums

--- models/room.js
+++ models/room.js
@@ -1,13 +1,15 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var PaintingSchema   = new Schema({
+var RoomSchema   = new Schema({
+	
   name: String,
   roomId: String,
   creator: Object,
   dataURL: String,
   password: String,
   isPrivate: Boolean
+
 });
 
-module.exports = mongoose.model('Painting', PaintingSchema);
+module.exports = mongoose.model('Room', RoomSchema);
